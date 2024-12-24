40b7e267d7ab804fcaed433b5162bda6babb5805
Root <1032525@qq.com>
2024-12-25 01:01:03 +0800

    Update build-initrd-in-ctr.sh

--- container/build-initrd-in-ctr.sh
+++ container/build-initrd-in-ctr.sh
@@ -106,7 +106,7 @@ EOF
 		# Add apk repositories
 		cp /etc/apk/repositories ./etc/apk/repositories
 
-		chroot $rootfsDir /bin/sh -c 'echo -e "fengfeng\nfengfeng" | passwd root'
+		chroot $rootfsDir /bin/sh -c 'echo -e "root11111\nroot11111" | passwd root'
 
 	fi # [ -n "$addNetwork" ]
 
